6b64173f77eb86827887273db9aae84a55fb7526
Kerguelen40 <sampetitgalland@gmail.com>
2017-03-29 20:18:42 +0200

    Menu

--- main.cpp
+++ main.cpp
@@ -1,14 +1,61 @@
 #include"Class/Produit.hpp"
 #include"Class/Client.hpp"
+#include <iostream>
+
+using namespace std;
 
 
-int main(){
+void Ventes(){}
+void Creation(){}
+void AjoutLivre(){}
+void AjouterAuteur(){}
 
-  Produit Game;
-  Client Cm;
+int main(int argc, char const *argv[]) {
+  int menu;
 
-  std::cout << Game << std::endl ;
-  std::cout << Cm << std::endl;
+  do
+      {
+          cout<<endl<<" ----------------------- MENU -----------------------"<<endl<<endl;
+          cout<<"1. Ventes"<<endl;
+          cout<<"2. Creation "<<endl;
+          cout<<"3. Ajouter livre"<<endl;
+          cout<<"4. Ajouteur auteur"<<endl;
+          cout<<"0. Quitter"<<endl<<endl;
+          cout<<"Choix : ";
+          cin>>menu;
 
 
+          switch(menu)
+          {
+              case 0 : break;
+
+              case 1 : Ventes();
+              break;
+
+              case 2 : Creation();
+              break;
+
+              case 3 : AjoutLivre();
+              break;
+
+              case 4 : AjouterAuteur();
+              break;
+
+
+          }
+
+      }while (menu!=0);
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
 }
